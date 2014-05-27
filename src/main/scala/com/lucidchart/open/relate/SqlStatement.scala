7885475add8b19f8357a70ce7a8b317a420e2505
Gregg <greggory.hz@gmail.com>
2014-05-27 14:08:20 -0600

    Changed optional function names to avoid type issues.

--- src/main/scala/com/lucidchart/open/relate/SqlStatement.scala
+++ src/main/scala/com/lucidchart/open/relate/SqlStatement.scala
@@ -12,34 +12,34 @@ import scala.reflect.ClassTag
 object SqlTypes {
   def bigDecimal(name: String, value: BigDecimal)(implicit stmt: SqlStatement) = stmt.bigDecimal(name, value)
   def bigDecimal(name: String, value: JBigDecimal)(implicit stmt: SqlStatement) = stmt.bigDecimal(name, value)
-  def bigDecimal[A](name: String, value: Option[A])(implicit stmt: SqlStatement, bd: BigDecimalLike[A]) = stmt.bigDecimal(name, value)
+  def bigDecimalOption[A](name: String, value: Option[A])(implicit stmt: SqlStatement, bd: BigDecimalLike[A]) = stmt.bigDecimalOption(name, value)
   def bigInt(name: String, value: BigInt)(implicit stmt: SqlStatement) = stmt.bigInt(name, value)
   def bigInt(name: String, value: JBigInt)(implicit stmt: SqlStatement) = stmt.bigInt(name, value)
-  def bigInt[A](name: String, value: Option[A])(implicit stmt: SqlStatement, bi: BigIntLike[A]) = stmt.bigInt(name, value)
+  def bigIntOption[A](name: String, value: Option[A])(implicit stmt: SqlStatement, bi: BigIntLike[A]) = stmt.bigIntOption(name, value)
   def bool(name: String, value: Boolean)(implicit stmt: SqlStatement) = stmt.bool(name, value)
-  def bool(name: String, value: Option[Boolean])(implicit stmt: SqlStatement) = stmt.bool(name, value)
+  def boolOption[A](name: String, value: Option[Boolean])(implicit stmt: SqlStatement) = stmt.boolOption(name, value)
   def byte(name: String, value: Byte)(implicit stmt: SqlStatement) = stmt.byte(name, value)
-  def byte(name: String, value: Option[Byte])(implicit stmt: SqlStatement) = stmt.byte(name, value)
+  def byteOption(name: String, value: Option[Byte])(implicit stmt: SqlStatement) = stmt.byteOption(name, value)
   def char(name: String, value: Char)(implicit stmt: SqlStatement) = stmt.char(name, value)
-  def char(name: String, value: Option[Char])(implicit stmt: SqlStatement) = stmt.char(name, value)
+  def charOption(name: String, value: Option[Char])(implicit stmt: SqlStatement) = stmt.charOption(name, value)
   def date(name: String, value: Date)(implicit stmt: SqlStatement) = stmt.date(name, value)
-  def date(name: String, value: Option[Date])(implicit stmt: SqlStatement) = stmt.date(name, value)
+  def dateOption(name: String, value: Option[Date])(implicit stmt: SqlStatement) = stmt.dateOption(name, value)
   def double(name: String, value: Double)(implicit stmt: SqlStatement) = stmt.double(name, value)
-  def double(name: String, value: Option[Double])(implicit stmt: SqlStatement) = stmt.double(name, value)
+  def doubleOption(name: String, value: Option[Double])(implicit stmt: SqlStatement) = stmt.doubleOption(name, value)
   def float(name: String, value: Float)(implicit stmt: SqlStatement) = stmt.float(name, value)
-  def float(name: String, value: Option[Float])(implicit stmt: SqlStatement) = stmt.float(name, value)
+  def floatOption(name: String, value: Option[Float])(implicit stmt: SqlStatement) = stmt.floatOption(name, value)
   def int(name: String, value: Int)(implicit stmt: SqlStatement) = stmt.int(name, value)
-  def int(name: String, value: Option[Int])(implicit stmt: SqlStatement) = stmt.int(name, value)
+  def intOption(name: String, value: Option[Int])(implicit stmt: SqlStatement) = stmt.intOption(name, value)
   def long(name: String, value: Long)(implicit stmt: SqlStatement) = stmt.long(name, value)
-  def long(name: String, value: Option[Long])(implicit stmt: SqlStatement) = stmt.long(name, value)
+  def longOption(name: String, value: Option[Long])(implicit stmt: SqlStatement) = stmt.longOption(name, value)
   def short(name: String, value: Short)(implicit stmt: SqlStatement) = stmt.short(name, value)
-  def short(name: String, value: Option[Short])(implicit stmt: SqlStatement) = stmt.short(name, value)
+  def shortOption(name: String, value: Option[Short])(implicit stmt: SqlStatement) = stmt.shortOption(name, value)
   def string(name: String, value: String)(implicit stmt: SqlStatement) = stmt.string(name, value)
-  def string(name: String, value: Option[String])(implicit stmt: SqlStatement) = stmt.string(name, value)
+  def stringOption(name: String, value: Option[String])(implicit stmt: SqlStatement) = stmt.stringOption(name, value)
   def timestamp(name: String, value: Timestamp)(implicit stmt: SqlStatement) = stmt.timestamp(name, value)
-  def timestamp(name: String, value: Option[Timestamp])(implicit stmt: SqlStatement) = stmt.timestamp(name, value)
+  def timestampOption(name: String, value: Option[Timestamp])(implicit stmt: SqlStatement) = stmt.timestampOption(name, value)
   def uuid(name: String, value: UUID)(implicit stmt: SqlStatement) = stmt.uuid(name, value)
-  def uuid(name: String, value: Option[UUID])(implicit stmt: SqlStatement) = stmt.uuid(name, value)
+  def uuidOption(name: String, value: Option[UUID])(implicit stmt: SqlStatement) = stmt.uuidOption(name, value)
 
   trait BigDecimalLike[A] {
     def get(value: A): JBigDecimal
@@ -102,7 +102,7 @@ class SqlStatement(stmt: PreparedStatement, names: Map[String, Int]) {
   }
   def bigDecimal[X: ClassTag](name: String, values: TraversableOnce[JBigDecimal]): Unit = list[JBigDecimal](name, values, stmt.setBigDecimal _)
 
-  def bigDecimal[A](name: String, value: Option[A])(implicit bd: SqlTypes.BigDecimalLike[A]): Unit = {
+  def bigDecimalOption[A](name: String, value: Option[A])(implicit bd: SqlTypes.BigDecimalLike[A]): Unit = {
     value.map(d => bigDecimal(name, bd.get(d))).getOrElse(stmt.setNull(names(name), Types.DECIMAL))
   }
 
@@ -126,7 +126,7 @@ class SqlStatement(stmt: PreparedStatement, names: Map[String, Int]) {
   }
   def bigInt[X: ClassTag](name: String, values: TraversableOnce[JBigInt]): Unit = list[JBigDecimal](name, values.map(new JBigDecimal(_)), stmt.setBigDecimal _)
 
-  def bigInt[A](name: String, value: Option[A])(implicit bd: SqlTypes.BigIntLike[A]): Unit = {
+  def bigIntOption[A](name: String, value: Option[A])(implicit bd: SqlTypes.BigIntLike[A]): Unit = {
     value.map(i => bigInt(name, bd.get(i))).getOrElse(stmt.setNull(names(name), Types.BIGINT))
   }
 
@@ -140,7 +140,7 @@ class SqlStatement(stmt: PreparedStatement, names: Map[String, Int]) {
   }
   def bool(name: String, values: TraversableOnce[Boolean]): Unit = list[Boolean](name, values, stmt.setBoolean _)
 
-  def bool(name: String, value: Option[Boolean]): Unit = {
+  def boolOption(name: String, value: Option[Boolean]): Unit = {
     value.map(bool(name, _)).getOrElse(stmt.setNull(names(name), Types.BOOLEAN))
   }
 
@@ -154,7 +154,7 @@ class SqlStatement(stmt: PreparedStatement, names: Map[String, Int]) {
   }
   def byte(name: String, values: TraversableOnce[Byte]): Unit = list[Byte](name, values, stmt.setByte _)
 
-  def byte(name: String, value: Option[Byte]): Unit = {
+  def byteOption(name: String, value: Option[Byte]): Unit = {
     value.map(byte(name, _)).getOrElse(stmt.setNull(names(name), Types.TINYINT))
   }
 
@@ -168,7 +168,7 @@ class SqlStatement(stmt: PreparedStatement, names: Map[String, Int]) {
   }
   def char(name: String, values: TraversableOnce[Char]): Unit = list[String](name, values.map(_.toString), stmt.setString _)
 
-  def char(name: String, value: Option[Char]): Unit = {
+  def charOption(name: String, value: Option[Char]): Unit = {
     value.map(char(name, _)).getOrElse(stmt.setNull(names(name), Types.CHAR))
   }
 
@@ -183,7 +183,7 @@ class SqlStatement(stmt: PreparedStatement, names: Map[String, Int]) {
   }
   def date(name: String, values: TraversableOnce[Date]): Unit = list[SqlDate](name, values.map{ d: Date => new SqlDate(d.getTime)}, stmt.setDate _)
 
-  def date(name: String, value: Option[Date]): Unit = {
+  def dateOption(name: String, value: Option[Date]): Unit = {
     value.map(date(name, _)).getOrElse(stmt.setNull(names(name), Types.DATE))
   }
 
@@ -197,7 +197,7 @@ class SqlStatement(stmt: PreparedStatement, names: Map[String, Int]) {
   }
   def double(name: String, values: TraversableOnce[Double]): Unit = list[Double](name, values, stmt.setDouble _)
 
-  def double(name: String, value: Option[Double]): Unit = {
+  def doubleOption(name: String, value: Option[Double]): Unit = {
     value.map(double(name, _)).getOrElse(stmt.setNull(names(name), Types.DOUBLE))
   }
 
@@ -211,7 +211,7 @@ class SqlStatement(stmt: PreparedStatement, names: Map[String, Int]) {
   }
   def float(name: String, values: TraversableOnce[Float]): Unit = list[Float](name, values, stmt.setFloat _)
 
-  def float(name: String, value: Option[Float]): Unit = {
+  def floatOption(name: String, value: Option[Float]): Unit = {
     value.map(float(name, _)).getOrElse(stmt.setNull(names(name), Types.FLOAT))
   }
 
@@ -225,7 +225,7 @@ class SqlStatement(stmt: PreparedStatement, names: Map[String, Int]) {
   }
   def int(name: String, values: TraversableOnce[Int]): Unit = list[Int](name, values, stmt.setInt _)
 
-  def int(name: String, value: Option[Int]): Unit = {
+  def intOption(name: String, value: Option[Int]): Unit = {
     value.map(int(name, _)).getOrElse(stmt.setNull(names(name), Types.INTEGER))
   }
 
@@ -239,7 +239,7 @@ class SqlStatement(stmt: PreparedStatement, names: Map[String, Int]) {
   }
   def long(name: String, values: TraversableOnce[Long]): Unit = list[Long](name, values, stmt.setLong _)
 
-  def long(name: String, value: Option[Long]): Unit = {
+  def longOption(name: String, value: Option[Long]): Unit = {
     value.map(long(name, _)).getOrElse(stmt.setNull(names(name), Types.BIGINT))
   }
 
@@ -253,7 +253,7 @@ class SqlStatement(stmt: PreparedStatement, names: Map[String, Int]) {
   }
   def short(name: String, values: TraversableOnce[Short]): Unit = list[Short](name, values, stmt.setShort _)
 
-  def short(name: String, value: Option[Short]): Unit = {
+  def shortOption(name: String, value: Option[Short]): Unit = {
     value.map(short(name, _)).getOrElse(stmt.setNull(names(name), Types.SMALLINT))
   }
 
@@ -265,17 +265,10 @@ class SqlStatement(stmt: PreparedStatement, names: Map[String, Int]) {
   def string(name: String, value: String): Unit = {
     stmt.setString(names(name), value)
   }
-  // def string(name: String, values: TraversableOnce[String]): Unit = {
-  //   var i = 0
-  //   val iterator = values.toIterator
-  //   while (iterator.hasNext) {
-  //     string(name + "_" + i, iterator.next())
-  //     i += 1
-  //   } 
-  // }
+
   def string(name: String, values: TraversableOnce[String]): Unit = list[String](name, values, stmt.setString _)
 
-  def string(name: String, value: Option[String]): Unit = {
+  def stringOption(name: String, value: Option[String]): Unit = {
     value.map(string(name, _)).getOrElse(stmt.setNull(names(name), Types.VARCHAR))
   }
 
@@ -289,7 +282,7 @@ class SqlStatement(stmt: PreparedStatement, names: Map[String, Int]) {
   }
   def timestamp(name: String, values: TraversableOnce[Timestamp]): Unit = list[Timestamp](name, values, stmt.setTimestamp _)
 
-  def timestamp(name: String, value: Option[Timestamp]): Unit = {
+  def timestampOption(name: String, value: Option[Timestamp]): Unit = {
     value.map(timestamp(name, _)).getOrElse(stmt.setNull(names(name), Types.TIMESTAMP))
   }
 
@@ -304,7 +297,7 @@ class SqlStatement(stmt: PreparedStatement, names: Map[String, Int]) {
   }
   def uuid(name: String, values: TraversableOnce[UUID]): Unit = list[String](name, values.map(_.toString), stmt.setString _)
 
-  def uuid(name: String, value: Option[UUID]): Unit = {
+  def uuidOption(name: String, value: Option[UUID]): Unit = {
     value.map(uuid(name, _)).getOrElse(stmt.setNull(names(name), Types.VARCHAR))
   }
 
