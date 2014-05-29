79216dd37f2fe87c1fa23a1a74abc37403b937c0
msiebert <mark@lucidchart.com>
2014-05-29 13:28:36 -0600

    added in single column parser options

--- src/main/scala/com/lucidchart/open/relate/RowParser.scala
+++ src/main/scala/com/lucidchart/open/relate/RowParser.scala
@@ -5,4 +5,10 @@ object RowParser {
   def apply[A](f: (SqlResult) => A) = new RowParser[A] {
     def apply(row: SqlResult) = f(row)
   }
+
+  def bigInt(columnLabel: String) = RowParser { row => row.bigInt(columnLabel) }
+  def date(columnLabel: String) = RowParser { row => row.date(columnLabel) }
+  def int(columnLabel: String) = RowParser { row => row.int(columnLabel) }
+  def long(columnLabel: String) = RowParser { row => row.long(columnLabel) }
+  def string(columnLabel: String) = RowParser { row => row.string(columnLabel) }
 }
