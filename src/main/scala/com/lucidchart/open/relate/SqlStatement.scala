7e256810ced8cd3c895f570587418d995c667ec1
Gregg <greggory.hz@gmail.com>
2014-05-30 09:30:56 -0600

    changed names to help understand semantic meaning.

--- src/main/scala/com/lucidchart/open/relate/SqlStatement.scala
+++ src/main/scala/com/lucidchart/open/relate/SqlStatement.scala
@@ -41,7 +41,7 @@ class SqlStatement(val stmt: PreparedStatement, val names: scala.collection.Map[
    * @param value the BigDecimal to put in the query
    */
   def bigDecimal(name: String, value: BigDecimal) = insert(name, value.bigDecimal, stmt.setBigDecimal _)
-  def bigDecimal(name: String, values: TraversableOnce[BigDecimal]): Unit = list[JBigDecimal](name, values.map(_.bigDecimal), stmt.setBigDecimal _)
+  def bigDecimals(name: String, values: TraversableOnce[BigDecimal]): Unit = list[JBigDecimal](name, values.map(_.bigDecimal), stmt.setBigDecimal _)
 
   /**
    * Set a Java BigDecimal in the PreparedStatement
@@ -49,7 +49,7 @@ class SqlStatement(val stmt: PreparedStatement, val names: scala.collection.Map[
    * @param value the BigDecimal to put in the query
    */
   def bigDecimal(name: String, value: JBigDecimal) = insert(name, value, stmt.setBigDecimal _)
-  def bigDecimal[X: ClassTag](name: String, values: TraversableOnce[JBigDecimal]): Unit = list[JBigDecimal](name, values, stmt.setBigDecimal _)
+  def bigDecimals[X: ClassTag](name: String, values: TraversableOnce[JBigDecimal]): Unit = list[JBigDecimal](name, values, stmt.setBigDecimal _)
   def bigDecimalOption[A](name: String, value: Option[A])(implicit bd: Query.BigDecimalLike[A]): Unit = {
     value.map(d => bigDecimal(name, bd.get(d))).getOrElse(insert(name, Types.DECIMAL, stmt.setNull _))
   }
@@ -60,7 +60,7 @@ class SqlStatement(val stmt: PreparedStatement, val names: scala.collection.Map[
    * @param value the BigInt to put into the query
    */
   def bigInt(name: String, value: BigInt) = insert(name, new JBigDecimal(value.bigInteger), stmt.setBigDecimal _)
-  def bigInt(name: String, values: TraversableOnce[BigInt]): Unit = list[JBigDecimal](name, values.map{ i: BigInt => new JBigDecimal(i.bigInteger) }, stmt.setBigDecimal _)
+  def bigInts(name: String, values: TraversableOnce[BigInt]): Unit = list[JBigDecimal](name, values.map{ i: BigInt => new JBigDecimal(i.bigInteger) }, stmt.setBigDecimal _)
 
   /**
    * Set a Java BigInteger in the PreparedStatement
@@ -68,7 +68,7 @@ class SqlStatement(val stmt: PreparedStatement, val names: scala.collection.Map[
    * @param value the BigInteger to put in the query
    */
   def bigInt(name: String, value: JBigInt) = insert(name, new JBigDecimal(value), stmt.setBigDecimal _)
-  def bigInt[X: ClassTag](name: String, values: TraversableOnce[JBigInt]): Unit = list[JBigDecimal](name, values.map(new JBigDecimal(_)), stmt.setBigDecimal _)
+  def bigInts[X: ClassTag](name: String, values: TraversableOnce[JBigInt]): Unit = list[JBigDecimal](name, values.map(new JBigDecimal(_)), stmt.setBigDecimal _)
   def bigIntOption[A](name: String, value: Option[A])(implicit bd: Query.BigIntLike[A]): Unit = {
     value.map(i => bigInt(name, bd.get(i))).getOrElse(insert(name, Types.BIGINT, stmt.setNull _))
   }
@@ -79,7 +79,7 @@ class SqlStatement(val stmt: PreparedStatement, val names: scala.collection.Map[
    * @param value the Boolean to put in the query
    */
   def bool(name: String, value: Boolean) = insert(name, value, stmt.setBoolean _)
-  def bool(name: String, values: TraversableOnce[Boolean]): Unit = list[Boolean](name, values, stmt.setBoolean _)
+  def bools(name: String, values: TraversableOnce[Boolean]): Unit = list[Boolean](name, values, stmt.setBoolean _)
   def boolOption(name: String, value: Option[Boolean]): Unit = {
     value.map(bool(name, _)).getOrElse(insert(name, Types.BOOLEAN, stmt.setNull _))
   }
@@ -90,7 +90,7 @@ class SqlStatement(val stmt: PreparedStatement, val names: scala.collection.Map[
    * @param value the Byte to put in the query
    */
   def byte(name: String, value: Byte) = insert(name, value, stmt.setByte _)
-  def byte(name: String, values: TraversableOnce[Byte]): Unit = list[Byte](name, values, stmt.setByte _)
+  def bytes(name: String, values: TraversableOnce[Byte]): Unit = list[Byte](name, values, stmt.setByte _)
   def byteOption(name: String, value: Option[Byte]): Unit = {
     value.map(byte(name, _)).getOrElse(insert(name, Types.TINYINT, stmt.setNull _))
   }
@@ -112,7 +112,7 @@ class SqlStatement(val stmt: PreparedStatement, val names: scala.collection.Map[
    * @param value the Char to put in the query
    */
   def char(name: String, value: Char) = insert(name, value.toString, stmt.setString _)
-  def char(name: String, values: TraversableOnce[Char]): Unit = list[String](name, values.map(_.toString), stmt.setString _)
+  def chars(name: String, values: TraversableOnce[Char]): Unit = list[String](name, values.map(_.toString), stmt.setString _)
   def charOption(name: String, value: Option[Char]): Unit = {
     value.map(char(name, _)).getOrElse(insert(name, Types.CHAR, stmt.setNull _))
   }
@@ -123,7 +123,7 @@ class SqlStatement(val stmt: PreparedStatement, val names: scala.collection.Map[
    * @param value the Date to put in the query
    */
   def date(name: String, value: Date) = insert(name, new Timestamp(value.getTime), stmt.setTimestamp _)
-  def date(name: String, values: TraversableOnce[Date]): Unit = list[Timestamp](name, values.map{ d: Date => new Timestamp(d.getTime)}, stmt.setTimestamp _)
+  def dates(name: String, values: TraversableOnce[Date]): Unit = list[Timestamp](name, values.map{ d: Date => new Timestamp(d.getTime)}, stmt.setTimestamp _)
   def dateOption(name: String, value: Option[Date]): Unit = {
     value.map(date(name, _)).getOrElse(insert(name, Types.DATE, stmt.setNull _))
   }
@@ -134,7 +134,7 @@ class SqlStatement(val stmt: PreparedStatement, val names: scala.collection.Map[
    * @param value the Double to put in the query
    */
   def double(name: String, value: Double) = insert(name, value, stmt.setDouble _)
-  def double(name: String, values: TraversableOnce[Double]): Unit = list[Double](name, values, stmt.setDouble _)
+  def doubles(name: String, values: TraversableOnce[Double]): Unit = list[Double](name, values, stmt.setDouble _)
   def doubleOption(name: String, value: Option[Double]): Unit = {
     value.map(double(name, _)).getOrElse(insert(name, Types.DOUBLE, stmt.setNull _))
   }
@@ -145,7 +145,7 @@ class SqlStatement(val stmt: PreparedStatement, val names: scala.collection.Map[
    * @param value the Float to put in the query
    */
   def float(name: String, value: Float) = insert(name, value, stmt.setFloat _)
-  def float(name: String, values: TraversableOnce[Float]): Unit = list[Float](name, values, stmt.setFloat _)
+  def floats(name: String, values: TraversableOnce[Float]): Unit = list[Float](name, values, stmt.setFloat _)
   def floatOption(name: String, value: Option[Float]): Unit = {
     value.map(float(name, _)).getOrElse(insert(name, Types.FLOAT, stmt.setNull _))
   }
@@ -156,7 +156,7 @@ class SqlStatement(val stmt: PreparedStatement, val names: scala.collection.Map[
    * @param value the int to put in the query
    */
   def int(name: String, value: Int) = insert(name, value, stmt.setInt _)
-  def int(name: String, values: TraversableOnce[Int]): Unit = list[Int](name, values, stmt.setInt _)
+  def ints(name: String, values: TraversableOnce[Int]): Unit = list[Int](name, values, stmt.setInt _)
   def intOption(name: String, value: Option[Int]): Unit = {
     value.map(int(name, _)).getOrElse(insert(name, Types.INTEGER, stmt.setNull _))
   }
@@ -167,7 +167,7 @@ class SqlStatement(val stmt: PreparedStatement, val names: scala.collection.Map[
    * @param value the Long to put in the query
    */
   def long(name: String, value: Long) = insert(name, value, stmt.setLong)
-  def long(name: String, values: TraversableOnce[Long]): Unit = list[Long](name, values, stmt.setLong _)
+  def longs(name: String, values: TraversableOnce[Long]): Unit = list[Long](name, values, stmt.setLong _)
   def longOption(name: String, value: Option[Long]): Unit = {
     value.map(long(name, _)).getOrElse(insert(name, Types.BIGINT, stmt.setNull _))
   }
@@ -178,7 +178,7 @@ class SqlStatement(val stmt: PreparedStatement, val names: scala.collection.Map[
    * @param value the Short to put in the query
    */
   def short(name: String, value: Short) = insert(name, value, stmt.setShort _)
-  def short(name: String, values: TraversableOnce[Short]): Unit = list[Short](name, values, stmt.setShort _)
+  def shorts(name: String, values: TraversableOnce[Short]): Unit = list[Short](name, values, stmt.setShort _)
   def shortOption(name: String, value: Option[Short]): Unit = {
     value.map(short(name, _)).getOrElse(insert(name, Types.SMALLINT, stmt.setNull _))
   }
@@ -189,7 +189,7 @@ class SqlStatement(val stmt: PreparedStatement, val names: scala.collection.Map[
    * @param value the value to put in the query
    */
   def string(name: String, value: String) = insert(name, value, stmt.setString _)
-  def string(name: String, values: TraversableOnce[String]): Unit = list[String](name, values, stmt.setString _)
+  def strings(name: String, values: TraversableOnce[String]): Unit = list[String](name, values, stmt.setString _)
   def stringOption(name: String, value: Option[String]): Unit = {
     value.map(string(name, _)).getOrElse(insert(name, Types.VARCHAR, stmt.setNull _))
   }
@@ -200,7 +200,7 @@ class SqlStatement(val stmt: PreparedStatement, val names: scala.collection.Map[
    * @param value the Timestamp to put into the query
    */
   def timestamp(name: String, value: Timestamp) = insert(name, value, stmt.setTimestamp)
-  def timestamp(name: String, values: TraversableOnce[Timestamp]): Unit = list[Timestamp](name, values, stmt.setTimestamp _)
+  def timestamps(name: String, values: TraversableOnce[Timestamp]): Unit = list[Timestamp](name, values, stmt.setTimestamp _)
   def timestampOption(name: String, value: Option[Timestamp]): Unit = {
     value.map(timestamp(name, _)).getOrElse(insert(name, Types.TIMESTAMP, stmt.setNull _))
   }
@@ -211,7 +211,7 @@ class SqlStatement(val stmt: PreparedStatement, val names: scala.collection.Map[
    * @param value the UUID to put in the query
    */
   def uuid(name: String, value: UUID) = insert(name, ByteHelper.uuidToByteArray(value), stmt.setBytes _)
-  def uuid(name: String, values: TraversableOnce[UUID]): Unit = list[Array[Byte]](name, values.map(ByteHelper.uuidToByteArray(_)), stmt.setBytes _)
+  def uuids(name: String, values: TraversableOnce[UUID]): Unit = list[Array[Byte]](name, values.map(ByteHelper.uuidToByteArray(_)), stmt.setBytes _)
   def uuidOption(name: String, value: Option[UUID]): Unit = {
     value.map(uuid(name, _)).getOrElse(insert(name, Types.VARCHAR, stmt.setNull _))
   }
