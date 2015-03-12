40e176617280e836ab5a6c6a7babbef3e04a47e5
rbanikaz <rezapb@hotmail.com>
2015-03-12 00:50:36 -0700

    quick and dirty support for #${ } interpolation (for dynamically changing table names, order clause, etc)

--- src/main/scala/com/lucidchart/open/relate/interp/InterpolatedQuery.scala
+++ src/main/scala/com/lucidchart/open/relate/interp/InterpolatedQuery.scala
@@ -3,6 +3,8 @@ package com.lucidchart.open.relate.interp
 import com.lucidchart.open.relate.Sql
 import java.sql.PreparedStatement
 
+import scala.collection.mutable.ListBuffer
+
 class InterpolatedQuery(protected val parsedQuery: String, protected val params: Seq[Parameter]) extends Sql with MultipleParameter {
 
   def +(query: InterpolatedQuery) = new InterpolatedQuery(parsedQuery + query.parsedQuery, params ++ query.params)
@@ -17,12 +19,20 @@ object InterpolatedQuery {
 
   def fromParts(parts: Seq[String], params: Seq[Parameter]) = {
     val stringBuilder = new StringBuilder()
+    val listBuf = new ListBuffer[Parameter]()
+
     parts.zip(params).foreach { case (part, param) =>
-      stringBuilder ++= part
-      param.appendPlaceholders(stringBuilder)
+      if (part.length > 0 && part.takeRight(1) == "#") {
+        stringBuilder ++= part.dropRight(1)
+        stringBuilder ++= param.toString
+      } else {
+        stringBuilder ++= part
+        param.appendPlaceholders(stringBuilder)
+        listBuf += param
+      }
     }
     stringBuilder ++= parts.last
-    new InterpolatedQuery(stringBuilder.toString(), params)
+    new InterpolatedQuery(stringBuilder.toString(), listBuf)
   }
 
 }
